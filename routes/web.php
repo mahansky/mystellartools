d8db3efd9818e0564a577acc61b09f7767ed0c60
mahansky <m.pro.mark@gmail.com>
2017-10-29 23:49:32 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,3 @@
 <?php
 
-Route::get('{path}', function () {
-    $routes = collect(Route::getRoutes()->getRoutesByName())->map(function ($route) {
-        return $route->uri;
-    });
-
-    return view('index', compact('routes'));
-})->where('path', '(.*)');
+Route::view('{path}', 'index')->where('path', '(.*)');
