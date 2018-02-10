bb1f041fd52033ebf941c170c5323dff0b421218
mahansky <m.pro.mark@gmail.com>
2018-02-10 13:29:29 +0100

    submit tx color classes

--- resources/assets/js/utils.js
+++ resources/assets/js/utils.js
@@ -42,13 +42,13 @@ export function syntaxHighlight (json) {
   json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
 
   return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-    let cls = 'number'
+    let cls = 'blue--text text--darken-2'
 
     if (/^"/.test(match)) {
       if (/:$/.test(match)) {
-        cls = 'key'
+        cls = 'red--text'
       } else {
-        cls = 'string'
+        cls = 'green--text'
       }
     }
 
