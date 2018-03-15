a801acce948d9a8d4d3e54fecf66694040246f1f
mahansky <m.pro.mark@gmail.com>
2018-03-15 14:54:11 +0100

    added ibm to known nodes

--- resources/assets/js/stellar/network_nodes.js
+++ resources/assets/js/stellar/network_nodes.js
@@ -4,9 +4,9 @@ export const groups = {
       background: '#c0eaff',
       highlight: {
         background: '#c0eaff',
-      }
+      },
     },
-  }
+  },
 }
 
 export const nodes = {
@@ -21,5 +21,44 @@ export const nodes = {
   'GABMKJM6I25XI4K7U6XWMULOUQIQ27BCTMLS6BYYSOWKTBUXVRJSXHYQ': {
     label: 'SDF Validator 3',
     group: 'sdf',
-  }
+  },
+  'GAXEJOMEVVD5OAAGOOZ4SXTTNR46C4V23XROHWRNVEGOTGOABO3ZVH7Z': {
+    label: 'FairX LLC US West',
+  },
+  'GARBCBH4YSHUJLYEPKEPMVYZIJ3ZSQR3QCJ245CWGY64X72JLN4A6RSG': {
+    label: 'IBM United States',
+    group: 'ibm',
+  },
+  'GAENPO2XRTTMAJXDWM3E3GAALNLG4HVMKJ4QF525TR25RI42YPEDULOW': {
+    label: 'IBM United Kingdom',
+    group: 'ibm',
+  },
+  'GCH3O5PTCZVR4G65W3B4XDKWI5V677HQB3QO7CW4YPVYDDFBE2GE7G6V': {
+    label: 'IBM India',
+    group: 'ibm',
+  },
+  'GBJ7T3BTLX2BP3T5Q4256PUF7JMDAB35LLO32QRDYE67TDDMN7H33GGE': {
+    label: 'IBM Hong Kong',
+    group: 'ibm',
+  },
+  'GAEEH4TBR7YQQWKJ2FIT57HXZZTMK2BX5LY4POJUYFSEZ7Y2ONHPPTES': {
+    label: 'IBM Italy',
+    group: 'ibm',
+  },
+  'GDRA72H7JWXAXWJKOONQOPH3JKNSH5MQ6BO5K74C3X6FO2G3OG464BPU': {
+    label: 'IBM Norway',
+    group: 'ibm',
+  },
+  'GB2HF2NHRKKFZYFDGD7MUENOYROOEK7SWYV2APYOODP6P7BUJTLILKIL': {
+    label: 'IBM Canada',
+    group: 'ibm',
+  },
+  'GBUJA3Z5TLAKLI5MEH4TETLXJBQVSVW74MNEKP5UUHTP3IMLNSUPOTVA': {
+    label: 'IBM Brazil',
+    group: 'ibm',
+  },
+  'GCKWUQGSVO45ZV3QK7POYL7HMFWDKWJVMFVEGUJKCAEVUITUCTQWFSM6': {
+    label: 'IBM Australia',
+    group: 'ibm',
+  },
 }
